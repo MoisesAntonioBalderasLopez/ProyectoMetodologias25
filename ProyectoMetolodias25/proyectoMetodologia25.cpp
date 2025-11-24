f80f7661b883953c5f81550a4e5c7c855f330fc7
MoisesAntonioBalderasLopez <moises.balderas@dsaix.com.mx>
2025-11-23 23:39:37 -0600

    agregar actualizacion metodos de busquedas

--- ProyectoMetolodias25/proyectoMetodologia25.cpp
+++ ProyectoMetolodias25/proyectoMetodologia25.cpp
@@ -49,11 +49,80 @@ void mostrarProductos(){
     }
 }
 
+void ordenarBurbuja(){
+    int i,j;
+    Producto aux;
+    for(i=0;i<tamanoProducto;i++){
+        for(j=0;j<tamanoProducto;j++){
+            if(productoLista[j].precio > productoLista[j+1].precio){
+                aux = productoLista[j];
+                productoLista[j] = productoLista[j+1];
+                productoLista[j+1] = aux;
+            }
+        }
+    }
+}
+
+void ordenarSeleccion(){
+    int i,j,min;
+    Producto aux;
+    for(i=0;i<tamanoProducto;i++){
+        min = i;
+        for(j=i+1;j<tamanoProducto;j++){
+            if(productoLista[j].precio < productoLista[min].precio){
+                min = j;
+            }
+        }
+        aux = productoLista[i];
+        productoLista[i] = productoLista[min];
+        productoLista[min] = aux;
+    }
+}
+
+void ordenarInsercion(){
+    int i,pos;
+    Producto aux;
+    for(i=0;i<tamanoProducto;i++){
+        pos = i;
+        aux = productoLista[i];
+        while((pos>0) && (productoLista[pos - 1].precio > aux.precio)){
+            productoLista[pos] = productoLista[pos -1];
+            pos--;
+        }
+        productoLista[pos] = aux;
+    }
+}
+
+void ordenarBurbujaOriginal(){
+    int arreglo[] = {6,3,5};
+    int i,j,aux;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(arreglo[j] > arreglo[j+1]){
+                aux = arreglo[j];
+                arreglo[j] = arreglo[j+1];
+                arreglo[j+1] = aux;
+            }
+        }
+        for(int x=0;x<3;x++){
+            cout << arreglo[x];
+        }
+        cout<<"\n";
+    }
+}
+
+
 int main()
 {
-    for(int i=0;i < 2;i++){
+    /*for(int i=0;i < 2;i++){
         registrarProductos();
     }
-    mostrarProductos();
+    mostrarProductos();*/
+    /*ordenarBurbuja();*/
+    /*ordenarSeleccion();*/
+    //ordenarBurbuja();
+    //ordenarSeleccion();
+    //ordenarInsercion();
+    ordenarBurbujaOriginal();
     return 0;
 }
